92b4767ac88cf8887e140989f6358ecd216ce77c
mohamedomara0 <mohamedomara674@gmail.com>
2020-09-10 15:05:08 +0200

    Updates

---
+++ precache-manifest.99ae3b3c093d31a43e6b24f8f40b61fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7984c1894ff32d394f4e45f980cb58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e62c9fbafa823837ffb",
+    "url": "/static/css/main.02d7656c.chunk.css"
+  },
+  {
+    "revision": "e92ffa0cf6c4030c21f4",
+    "url": "/static/js/2.65c2d681.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.65c2d681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e62c9fbafa823837ffb",
+    "url": "/static/js/main.56ebad13.chunk.js"
+  },
+  {
+    "revision": "961a203a3aea5771c3d5",
+    "url": "/static/js/runtime-main.19ff018e.js"
+  }
+]);
